5ffa3e39223230215270ff26528271cd5c11c090
Ash <01tripster@gmail.com>
2025-08-10 16:58:22 +0530

    feat: basic ping command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,21 @@ fun main(args: Array<String>) {
     System.err.println("Logs from your program will appear here!")
 
     // Uncomment this block to pass the first stage
-     val serverSocket = ServerSocket(6379)
+    val serverSocket = ServerSocket(6379)
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    serverSocket.reuseAddress = true
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    val clientSocket = serverSocket.accept() // Wait for connection from client.
+    println("accepted new connection")
+
+    val outputStream = clientSocket.outputStream
+
+    val pongResponse = "+PONG\r\n"
+    outputStream.write(pongResponse.toByteArray())
+    outputStream.flush()
+
+    clientSocket.close()
+    serverSocket.close()
 }
